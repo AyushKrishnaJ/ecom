0d2e5e8b6aa6e2ca83fca038d62154cd448e2828
Akashtj2004 <1dergaming123@gmail.com>
2025-01-25 20:27:25 +0530

    hero section added

--- ECOM_FRONTEND/src/App.jsx
+++ ECOM_FRONTEND/src/App.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Login from './Pages/Login'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home'
+import Navbar from './components/Navbar'
 
 const App = () => {
+  const [isauth,setIsauth]=useState(false)
+  
   return (
+    
     <div>
-      <Login/>
+      
+      {isauth && <Navbar/>}
+      <Routes>
+        <Route path='/' element={isauth ? <Navigate to="/home" />:<Login isauth={setIsauth}/>}/>
+        
+        <Route path='/home' element={<Home/>}/>
+      </Routes>
     </div>
   )
 }
---
+++ ECOM_FRONTEND/src/Pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Home.css'
+const Home = () => {
+  return (
+    <div>
+      <div className="hero">
+        <div className="left">
+          <h1>Welcome to <br /> <span>TAAS</span> Store</h1>
+          <p>"Quality Products, Exceptional Prices – Right Here, Right Now."</p>
+          <button>Shop Now</button>
+        </div>
+        <div className="right">
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
