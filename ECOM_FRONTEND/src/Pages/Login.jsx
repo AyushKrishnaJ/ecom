1030e2ff6f1e12104f4395909c2a98d49a7062af
Akashtj2004 <1dergaming123@gmail.com>
2025-01-25 12:49:35 +0530

    Login page updated

--- ECOM_FRONTEND/src/Pages/Login.jsx
+++ ECOM_FRONTEND/src/Pages/Login.jsx
@@ -2,20 +2,43 @@ import React, { useState } from 'react'
 import './Login.css'
 const Login = () => {
   const Handlelogin=(e)=>{
-    e.preventDefault()
+    const fetchlogin= async ()=>{
+      
+      try{
+        const response = await axios.post("http://localhost:5173/api/auth/login",{email,password});
+        console.log("Login successful:", response.data);
+        alert("Login successful!");
+      }
+      catch(err){
+        console.error("error during login",err)
+        setError("Invalid Credentials")
+      }finally{
+        setLoading(false)
+      }
+
+    }
+    fetchlogin();
+    setLoading(true)
+    e.preventDefault();
+    setEmail("");
+    setPassword("");
 
   }
-    const [email,setEmail]=useState(null)
-    const [password,setPassword]=useState(null)
+    const [email,setEmail]=useState("")
+    const [password,setPassword]=useState("")
+    const [error,setError]=useState(null)
+    const [loading,setLoading]=useState(false)
   return (
     <div className='login'>
       <form onSubmit={(e)=>{Handlelogin(e)}} >
         <h1>Login Page</h1>
         <div className="input">
-            <input onChange={(e)=>{setEmail(e.target.value)}} type="email" value={email} placeholder='Enter your email' />
-            <input onChange={(e)=>{setPassword(e.target.value)}} type="password" value={password} placeholder='Password' />
+            <input required onChange={(e)=>{setEmail(e.target.value)}} type="email" value={email} placeholder='Email' />
+            <input required onChange={(e)=>{setPassword(e.target.value)}} type="password" value={password} placeholder='Password' />
             <button>Login</button>
         </div>
+        {loading && <p style={{ color: "green" }}>Loading...</p>}
+        {error && <p style={{ color: "red" }}>{error}</p>}
       </form>
     </div>
   )
