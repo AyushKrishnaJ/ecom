ea3370d399466576d7ec27ea57963683814afb0c
Tejas1346 <tejasshyampn@gmail.com>
2025-01-25 21:25:23 +0530

    linked pages

--- ECOM_FRONTEND/src/App.jsx
+++ ECOM_FRONTEND/src/App.jsx
@@ -3,6 +3,8 @@ import Login from './Pages/Login'
 import { Navigate, Route, Routes } from 'react-router-dom'
 import Home from './Pages/Home'
 import Navbar from './components/Navbar'
+import ProductComponent from './Pages/ProductListPage'
+import Product from './Pages/ProductDetails'
 
 const App = () => {
   const [isauth,setIsauth]=useState(false)
@@ -11,10 +13,11 @@ const App = () => {
     
     <div>
       
-      {isauth && <Navbar/>}
+      {isauth }
       <Routes>
-        <Route path='/' element={isauth ? <Navigate to="/home" />:<Login isauth={setIsauth}/>}/>
-        
+        <Route path='/' element={isauth ? <Navigate to="/home" />:<Login setIsauth={setIsauth}/>}/>
+          <Route path='/products' element={<ProductComponent/>}/>
+          <Route path='/product/:id' element ={<Product/>}/>
         <Route path='/home' element={<Home/>}/>
       </Routes>
     </div>
