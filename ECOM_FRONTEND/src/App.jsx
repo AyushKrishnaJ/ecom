b02062e305c4dd9c7d65542d85ee56799a7de93f
Tejas1346 <tejasshyampn@gmail.com>
2025-01-25 20:39:03 +0530

    added product

--- ECOM_FRONTEND/src/App.jsx
+++ ECOM_FRONTEND/src/App.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import {Routes, Route } from 'react-router-dom'
 import Login from './Pages/Login'
-
+import ProductComponent from './Pages/ProductListPage'
+import CartComponent from './Pages/CartComponent';
+import Product from './Pages/ProductDetails';
 const App = () => {
   return (
-    <div>
-      <Login/>
-    </div>
+    
+      <Routes>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/products' element={<ProductComponent/>}/>
+      <Route path="product/:id" element={<Product />} />
+      <Route path='/cart' element={<CartComponent/>}/>
+      </Routes>
+    
   )
 }
 
